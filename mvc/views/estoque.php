d93f3cb4c37980a29858febeb034b86f73916a29
Veridiana <veridiana17timoteo@gmail.com>
2023-04-09 00:37:15 -0300

    Adição de verificação de user e listagem

--- mvc/views/estoque.php
+++ mvc/views/estoque.php
@@ -1,3 +1,8 @@
+<?php
+    if (!isset($estoques)) {
+    
+?>
+
 <form action = "<?php echo APP . 'estoque/salvar'?>" method = "POST">
   <div class="form-row">
     <div class="form-group col-md-6">
@@ -17,3 +22,31 @@
   </div>
   <button type="submit" class="btn btn-primary">Adicionar</button>
 </form>
+
+<?php
+      }else{
+        foreach ($estoques as $as) {   
+
+        echo "
+        <div class='col-sm-2 d-flex align-items-stretch' style='margin-top: 2rem;'>
+        <div class='card corzinha'>
+        <div class='card-header' style='text-align: center;'>
+        <h5 class='card-title'>#{$as['nomeproduto']}</h5>
+        </div>
+        <div class='card-body' style='text-align: center'>
+        <h5>Quantidade: {$as['qttproduto']}</h5>
+        <h5>Preco: {$as['preco']}</h5>
+        <h5>Categoria: {$as['nomecateg']}</h5>
+        <a class='btn btn-danger' href = 'estoque/deletar/{$as['estoque_id']}'>Excluir</a>
+        <a class='btn btn-primary' href = 'estoque/update/{$as['estoque_id']}'>Editar</a>
+        </div>
+        </div>
+        </div>
+         
+        ";
+
+
+        }
+      }
+
+?>
