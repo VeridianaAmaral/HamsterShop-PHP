3fa444e1ebdbab1df98a820913c1daa2853e65a1
Veridiana <veridiana17timoteo@gmail.com>
2023-04-10 02:09:07 -0300

    Adicionando id do produto na query

--- mvc/models/Produto.php
+++ mvc/models/Produto.php
@@ -3,7 +3,7 @@
 class Produto extends Model{
 
 protected $tabela = "produto";
-protected $query = "select prod.descricao as nomeProduto, prod.preco as preco, categ.nome as nomeCateg
+protected $query = "select prod.id as id, prod.descricao as nomeProduto, prod.preco as preco, categ.nome as nomeCateg
 from  produto prod join categoria categ
     on categ.id = prod.categoria_id";
 }
